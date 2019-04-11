721fe0149c35ba1c3e840a18b7d35f5ab58ab94e
Thomas Lorntzen <cyrthomas1@gmail.com>
2019-04-11 09:44:03 +0200

    Test hook

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const {
   poweredByHandler
 } = require('./handlers.js')
 
+
 // For deployment to Heroku, the port needs to be set using ENV, so
 // we check for the port number in process.env
 app.set('port', (process.env.PORT || 9001))
