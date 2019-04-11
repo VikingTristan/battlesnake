d6da68f69ec63606c5a33fa56e53d077d1cbe46e
Thomas Lorntzen <cyrthomas1@gmail.com>
2019-04-11 12:42:09 +0200

    Avoid blocking cells in a clockwise manner

--- services/move.js
+++ services/move.js
@@ -1,5 +1,7 @@
 // Eventually other services here
 
+const Offset = 1;
+
 const createBoard = (height, width) => {
     let arr = [];
 
@@ -9,14 +11,14 @@ const createBoard = (height, width) => {
     }
 
     // Fill all cells with 0
-    for(i = 0; i < width+2; i++){
-        for (j = 0; j < height+2; j++){
+    for(i = 0; i < (width + (Offset * 2)); i++){
+        for (j = 0; j < (height + (Offset*2)); j++){
             arr[i][j] = 0;
         }
     }
 
     // Fill the outer edges with -1
-    for(i = 0; i < width+2; i++){
+    for(i = 0; i < (width + (Offset*2)); i++){
         arr[0][i] = -1;
         arr[i][0] = -1;
 
@@ -29,33 +31,70 @@ const createBoard = (height, width) => {
 
 const fillBoardWithFood = (board, foodArray) => {
     foodArray.forEach(food => {
-        board[food.y+1][food.x+1] = 1;
+        board[food.y + Offset][food.x + Offset] = 1;
     });
 }
 
 const fillBoardWithSnakes = (board, snakes) => {
     snakes.forEach(snake => {
         snake.body.forEach(body => {
-            board[body.y+1][body.x+1] = 2;
+            board[body.y + Offset][body.x + Offset] = 2;
         })
     });
 }
 
 const fillBoardWithOwnSnake = (board, ownSnake) => {
     ownSnake.body.forEach(body => {
-        board[body.y+1][body.x+1] = 3;
+        board[body.y + Offset][body.x + Offset] = 3;
     });
 }
 
-const generateMove = request => {
-    let nextMove = "right";
+// Check surrounds from head location at board and prioritize move accordingly (Prioritize apple over blank space)
+const checkSurroundings = (board, head) => {
 
+   let up = board[head.y - 1  + Offset][head.x  + Offset];
+   let right = board[head.y  + Offset][head.x + 1  + Offset];
+   let down = board[head.y + 1  + Offset][head.x  + Offset];
+   let left = board[head.y  + Offset][head.x - 1  + Offset];
+
+   let surroundings = [up,right,down,left]
+   return surroundings;
+}
+
+// If any of the neigbhouring cells are empty or food, that is a legal move
+const GetLegalMoveFromSurroundings = (board, surroundings) => {
+    for (i = 0; i < 4; i++){
+        if (surroundings[i] == 0 || surroundings[i] == 1){
+            switch (i) {
+                case 0:
+                return "up"
+                case 1:
+                return "right"
+                case 2:
+                return "down"
+                case 3:
+                return "left"
+                default:
+                return "up"
+            }
+        }
+    }
+}
+
+const generateMove = request => {
     let board = createBoard(request.body.board.width, request.body.board.height);
     fillBoardWithFood(board, request.body.board.food);
     fillBoardWithSnakes(board, request.body.board.snakes);
     fillBoardWithOwnSnake(board, request.body.you);
+
+    let surroundings = checkSurroundings(board, request.body.you.body[0]);
+    console.log(surroundings);
+
+    let nextMove = GetLegalMoveFromSurroundings(board, surroundings);
     // Do not walk into a wall...
-    console.log(board);
+    console.log(nextMove);
+    
+    //console.log(board);
     
     
     // Snake move logic here?
